8c07520d6fdf53dfa5d5efe152e828eee41439c0
Lonzieeee <wanderilorna@gmail.com>
2025-10-07 20:15:04 +0300

    vite config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -10,4 +9,9 @@ export default defineConfig({
     sourcemap: false,
   },
   base: '/',
+  preview: {
+    allowedHosts: ['nurseme.magical.africa'],
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  },
 })
